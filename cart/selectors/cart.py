0b7d0f57285b7777f95526675b99e43a9f32a7fd
MahshadAzizi <mahshad.azizi98@gmail.com>
2025-04-24 19:44:26 +0330

    fix: change cart selector to read data from cache

--- cart/selectors/cart.py
+++ cart/selectors/cart.py
@@ -1,10 +1,26 @@
 from typing import Optional
 
+from django.core.cache import cache
+from django.utils import timezone
+
 from cart.models import Cart
 
 
 class CartSelector:
     @staticmethod
     def get_active_cart(user) -> Optional[Cart]:
-        return Cart.objects.filter(user=user, is_active=True).first()
+        cart_key = f'cart_{user.id}_active'
+        cart = cache.get(cart_key)
+        if cart:
+            return cart
 
+        cart = Cart.objects.filter(user=user, is_active=True).first()
+        if cart:
+            if cart.expires_at <= timezone.now():
+                cart.cart_deactivate()
+                cache.delete(cart_key)
+                return None
+
+            ttl = (cart.expires_at - timezone.now()).total_seconds()
+            cache.set(cart_key, cart, timeout=ttl)
+        return cart
