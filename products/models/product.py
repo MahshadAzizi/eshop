b22cf32b4942f381e4ccef1ebc280656280b90fb
MahshadAzizi <mahshad.azizi98@gmail.com>
2025-04-24 13:47:30 +0330

    feat: add product model

---
+++ products/models/product.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=255,
+        db_index=True
+    )
+
+    description = models.TextField(
+        blank=True,
+    )
+
+    inventory = models.PositiveIntegerField()
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    is_active = models.BooleanField(
+        default=True,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return self.name
