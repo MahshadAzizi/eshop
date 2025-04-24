fe801558f0e18b967aab493b2bf81c9128096ba5
MahshadAzizi <mahshad.azizi98@gmail.com>
2025-04-24 14:54:21 +0330

    feat: add cart endpoint

---
+++ cart/api/views/cart.py
@@ -0,0 +1,19 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from cart.api.serializers import CartSerializer
+from cart.selectors.cart import CartSelector
+from cart.services.cart import CartService
+
+
+class CartView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        cart = CartSelector.get_active_cart(user=request.user)
+
+        if not cart:
+            cart = CartService.create_cart_for_user(request.user)
+        serializer = CartSerializer(cart)
+        return Response(serializer.data)
