aec2030aff41f5baa7a2f9bd10f4c25caf8a30fe
MahshadAzizi <mahshad.azizi98@gmail.com>
2025-04-24 18:09:07 +0330

    feat: add cart item serializer

--- cart/api/serializers/cart_item.py
+++ cart/api/serializers/cart_item.py
@@ -1,14 +1,18 @@
 from rest_framework import serializers
 
 from cart.models import CartItem
+from products.api.serializers import ProductSerializer
+from products.models import Product
 
 
 class CartItemSerializer(serializers.ModelSerializer):
-    product = serializers.StringRelatedField()
+    product = serializers.SlugRelatedField(queryset=Product.objects.all(), slug_field='id', write_only=True)
+    product_detail = ProductSerializer(source='product', read_only=True)
 
     class Meta:
         model = CartItem
         fields = [
             'product',
+            'product_detail',
             'quantity',
         ]
