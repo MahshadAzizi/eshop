d8de5d95fb2ce35fe5ea72db3c84de5f92f8164c
MahshadAzizi <mahshad.azizi98@gmail.com>
2025-04-24 19:44:04 +0330

    fix: change cart service to save in cache

--- cart/services/cart.py
+++ cart/services/cart.py
@@ -1,3 +1,5 @@
+from django.core.cache import cache
+
 from cart.models import Cart
 
 
@@ -5,6 +7,7 @@ class CartService:
     @staticmethod
     def create_cart_for_user(user):
         """Create a new active cart for a given user with a 30-minute expiration."""
-        return Cart.objects.create(
-            user=user,
-        )
+        cart = Cart.objects.create(user=user)
+        cart_key = f'cart_{user.id}_active'
+        cache.set(cart_key, cart, timeout=1800)
+        return cart
