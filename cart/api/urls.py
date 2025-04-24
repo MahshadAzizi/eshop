d3754887c6b4ace1d18ab601a2cf06cc5f9ff50e
MahshadAzizi <mahshad.azizi98@gmail.com>
2025-04-24 18:10:32 +0330

    feat: add cart items to url

--- cart/api/urls.py
+++ cart/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from cart.api.views import CartView
+from cart.api.views import CartView, CartItemsView
 
 urlpatterns = [
     path('', CartView.as_view(), name='cart'),
+    path('items', CartItemsView.as_view(), name='cart_items')
 ]
