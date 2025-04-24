c8327933152c4b00842483a0261c6399554f1d49
MahshadAzizi <mahshad.azizi98@gmail.com>
2025-04-24 14:56:32 +0330

    feat: add user to admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class SimpleUserAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'is_active', 'is_staff', 'date_joined')
+    readonly_fields = ('date_joined',)
